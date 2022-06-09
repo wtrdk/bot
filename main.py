1a372885a665b606ab63b63db920ed9cf9d12bd2
wtrdk <wouter.dijk@gmail.com>
2022-06-09 15:25:49 +0200

    fix

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import asyncio
 import telegram
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
 
 
 async def main():
     bot = telegram.Bot("TOKEN")
     async with bot:
-        print(await bot.get_me())
 
-
-if __name__ == '__main__':
-    asyncio.run(main())
+        print((await bot.get_updates())[0])
